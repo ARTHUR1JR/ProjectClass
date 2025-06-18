19148ae4d0e5b37682933e44de30484af39d9282
ARTHUR1JR <mukeshkumar100503@gmail.com>
2025-06-18 12:10:50 +0530

    intial project strucure

--- src/test/java/org/browserlaunch/BrowserLaunch.java
+++ src/test/java/org/browserlaunch/BrowserLaunch.java
@@ -3,7 +3,11 @@ package org.browserlaunch;
 public class BrowserLaunch {
 
 	public BrowserLaunch() {
-		System.out.println("gitttttttttttttttttttt");
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+		System.out.println("test5");
 	}
 
 }
